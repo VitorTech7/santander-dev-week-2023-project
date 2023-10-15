a5f9b20b619e2fa4589025f7d98bdc4f2c9dab0d
Vitor <vytroxalberto@gmail.com>
2023-10-15 14:39:44 -0300

    Initial commit

---
+++ src/main/java/com/diovitor/santanderdevweek2023project/SantanderDevWeek2023ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.diovitor.santanderdevweek2023project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2023ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2023ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diovitor/santanderdevweek2023project/SantanderDevWeek2023ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diovitor.santanderdevweek2023project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
